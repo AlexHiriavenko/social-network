468c7add28c564db4c7b481f010ed4d467355886
Viktor Melnik <126667387+Viktor-Melnik@users.noreply.github.com>
2023-06-28 09:58:31 +0300

    add redux (#5)

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <Provider store={store}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </Provider>
 );
