4022406d6e52533f657da7c47143d55b01d7f404
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-07-01 18:36:54 +0300

    header in progress

--- src/components/Header/HeaderSearch/HeaderSearch.jsx
+++ src/components/Header/HeaderSearch/HeaderSearch.jsx
@@ -47,23 +47,31 @@ function HeaderSearch() {
             >
                 {" "}
                 <Box sx={{ width: "320px" }}>
-                    <List>
-                        <ListItem sx={{ p: 0 }}>
-                            <Button
-                                onClick={handleBackdropClick}
-                                sx={{ minWidth: "44px", minHeight: "44px", borderRadius: "50%" }}
-                            >
-                                <ArrowBackIcon
-                                    style={{
-                                        color: "rgb(101, 103, 107)",
-                                        minWidth: "24px",
-                                    }}
-                                />
-                            </Button>
-                            <Search inputClass="header__drawer-searh-input"></Search>
-                        </ListItem>
-                        <Divider sx={{ mt: "12px" }}></Divider>
-                    </List>
+                    <Box sx={{ display: "flex", p: 1 }}>
+                        <Button
+                            onClick={handleBackdropClick}
+                            sx={{
+                                minWidth: "44px",
+                                minHeight: "44px",
+                                borderRadius: "50%",
+                                "&:hover": {
+                                    backgroundColor: "#F0F2F5",
+                                },
+                            }}
+                        >
+                            <ArrowBackIcon
+                                style={{
+                                    color: "rgb(101, 103, 107)",
+                                    minWidth: "24px",
+                                }}
+                            />
+                        </Button>
+                        <Search inputClass="header__drawer-searh-input"></Search>
+                    </Box>
+                    <Divider sx={{ mt: "4px" }}></Divider>
+                    {/* <List>
+                        <ListItem sx={{ p: 0 }}></ListItem>
+                    </List> */}
                 </Box>
             </Drawer>
         </>
