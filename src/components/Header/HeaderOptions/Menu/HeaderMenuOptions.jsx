717cb5c07c6eba6924892b0520e0aacbdc868c20
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-07-04 18:15:03 +0300

    header in progress

--- src/components/Header/HeaderOptions/Menu/HeaderMenuOptions.jsx
+++ src/components/Header/HeaderOptions/Menu/HeaderMenuOptions.jsx
@@ -1,65 +1,65 @@
-import AppsIcon from "@mui/icons-material/Apps";
-import React from "react";
-import { IconButton, Typography, Menu, Avatar, Tooltip, MenuItem } from "@mui/material";
+// import AppsIcon from "@mui/icons-material/Apps";
+// import React from "react";
+// import { IconButton, Typography, Menu, Avatar, Tooltip, MenuItem } from "@mui/material";
 
-function HeaderMenuOptions() {
-    const [anchorElMenu, setAnchorElMenu] = React.useState(null);
+// function HeaderMenuOptions() {
+//     const [anchorElMenu, setAnchorElMenu] = React.useState(null);
 
-    const toggleMenu = () =>
-        anchorElMenu
-            ? setAnchorElMenu(null)
-            : setAnchorElMenu(document.querySelector(".anchor-menu"));
+//     const toggleMenu = () =>
+//         anchorElMenu
+//             ? setAnchorElMenu(null)
+//             : setAnchorElMenu(document.querySelector(".anchor-menu"));
 
-    return (
-        <>
-            <Tooltip
-                title="Menu"
-                sx={{
-                    display: {
-                        xs: "none",
-                        lg: "flex",
-                    },
-                    p: { xs: "4px", sm: 1 },
-                }}
-            >
-                <IconButton onClick={toggleMenu}>
-                    <Avatar sx={{ bgcolor: "#F0F2F5", minWidth: "40px", minHeight: "40px" }}>
-                        <AppsIcon style={{ color: "black" }} />
-                    </Avatar>
-                </IconButton>
-            </Tooltip>
-            <Menu
-                sx={{ mt: "45px" }}
-                anchorEl={anchorElMenu}
-                anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                }}
-                transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                }}
-                // keepMounted
-                open={Boolean(anchorElMenu)}
-                onClose={toggleMenu}
-                slotProps={{
-                    paper: {
-                        className: "header__options-drop-menu",
-                    },
-                }}
-            >
-                <MenuItem onClick={toggleMenu}>
-                    <Typography textAlign="center">menu 1</Typography>
-                </MenuItem>
-                <MenuItem onClick={toggleMenu}>
-                    <Typography textAlign="center">menu 2</Typography>
-                </MenuItem>
-                <MenuItem onClick={toggleMenu}>
-                    <Typography textAlign="center">menu 3</Typography>
-                </MenuItem>
-            </Menu>
-        </>
-    );
-}
+//     return (
+//         <>
+//             <Tooltip
+//                 title="Menu"
+//                 sx={{
+//                     display: {
+//                         xs: "none",
+//                         lg: "flex",
+//                     },
+//                     p: { xs: "4px", sm: 1 },
+//                 }}
+//             >
+//                 <IconButton onClick={toggleMenu}>
+//                     <Avatar sx={{ bgcolor: "#F0F2F5", minWidth: "40px", minHeight: "40px" }}>
+//                         <AppsIcon style={{ color: "black" }} />
+//                     </Avatar>
+//                 </IconButton>
+//             </Tooltip>
+//             <Menu
+//                 sx={{ mt: "45px" }}
+//                 anchorEl={anchorElMenu}
+//                 anchorOrigin={{
+//                     vertical: "top",
+//                     horizontal: "right",
+//                 }}
+//                 transformOrigin={{
+//                     vertical: "top",
+//                     horizontal: "right",
+//                 }}
+//                 // keepMounted
+//                 open={Boolean(anchorElMenu)}
+//                 onClose={toggleMenu}
+//                 slotProps={{
+//                     paper: {
+//                         className: "header__options-drop-menu",
+//                     },
+//                 }}
+//             >
+//                 <MenuItem onClick={toggleMenu}>
+//                     <Typography textAlign="center">menu 1</Typography>
+//                 </MenuItem>
+//                 <MenuItem onClick={toggleMenu}>
+//                     <Typography textAlign="center">menu 2</Typography>
+//                 </MenuItem>
+//                 <MenuItem onClick={toggleMenu}>
+//                     <Typography textAlign="center">menu 3</Typography>
+//                 </MenuItem>
+//             </Menu>
+//         </>
+//     );
+// }
 
-export default HeaderMenuOptions;
+// export default HeaderMenuOptions;
