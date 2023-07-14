4a791a9be20157834bd5a8cdb7248044d6379941
Nazar Melnik <nazar97melnik@gmail.com>
2023-07-14 15:30:54 +0300

    finnished last changes with logic log in / log out

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import PrivateRoute from "./utils/router/PrivateRoute";
@@ -20,36 +21,51 @@ import {
   UserPage,
 } from "./pages/";
 import Header from "./components/Header/Header";
-import { useState } from "react";
+import { logIn } from "./redux/login.slice/login.slice";
 
 function App() {
-  const [mockAuth, setMockAuth] = useState(false);
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   const navigate = useNavigate();
-  const logIn = () => {
-    setMockAuth(true);
+
+  const handleLogIn = () => {
+    dispatch(logIn());
     navigate("/");
   };
 
   return (
     <>
-      {mockAuth && <Header />}
+      {isLoggedIn && <Header />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/watch" element={<Watch />} />
-        <Route path="/marketplace" element={<Marketplace />} />
-        <Route path="/groups" element={<Groups />} />
-        <Route path="*" element={<NotFound />} />z
+        <Route element={<PrivateRoute auth={isLoggedIn} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/watch" element={<Watch />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/profile" element={<Profile />}>
+            <Route path="/profile/" element={<ProfilePosts />} />
+            <Route path="/profile/about" element={<ProfileAbout />}>
+              <Route path="/profile/about/" element={<Overview />} />
+              <Route
+                path="/profile/about/employment"
+                element={<Employment />}
+              />
+              <Route path="/profile/about/places" element={<Places />} />
+              <Route path="/profile/about/contacts" element={<Contacts />} />
+            </Route>
+            <Route path="/profile/friends" element={<ProfileFriends />} />
+            <Route path="/profile/photos" element={<ProfilePhotos />} />
+          </Route>
+          <Route path="/user-page/:id" element={<UserPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
         <Route
           path="/login"
-          element={<LogIn auth={mockAuth} onClick={logIn} />}
+          element={<LogIn isLoggedIn={isLoggedIn} onClick={handleLogIn} />}
         />
       </Routes>
-      {mockAuth && (
-        <button onClick={() => setMockAuth(false)} className="tempBtn">
-          LogOut
-        </button>
-      )}
     </>
   );
 }
+
 export default App;
--- src/pages/LogIn/Login.jsx
+++ src/pages/LogIn/Login.jsx
@@ -11,9 +11,11 @@ import { useFormik } from "formik";
 import loginValidation from "./Validation/loginValidation";
 import RegisterModal from "./RegisterModal";
 import ForgotForm from "./ForgotForm";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logIn } from "../../redux/login.slice/login.slice";
+import { useNavigate } from "react-router-dom";
 export default function LogIn() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [registerModal, setRegisterModal] = useState(false);
   const handleRegisterModal = () => {
@@ -27,6 +29,7 @@ export default function LogIn() {
     validationSchema: loginValidation,
     onSubmit: () => {
       dispatch(logIn());
+      navigate("/");
       return console.log({
         email: loginForm.values.email,
         password: loginForm.values.password,
@@ -43,92 +46,95 @@ export default function LogIn() {
     setShowPassword(!showPassword);
   };
   const recentLogin = localStorage.getItem("recentLogin") || [];
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   return (
     <>
-      <section className="login-section">
-        <Container maxWidth="xl">
-          <div className="login-section__wrapper">
-            <div className="login-text__wrapper">
-              <h2 className="login-title">facebook</h2>
-              <p className="login-subtitle">
-                Facebook помогает вам всегда оставаться на связи и общаться со
-                своими знакомыми.
-              </p>
+      {isLoggedIn ? null : (
+        <section className="login-section">
+          <Container maxWidth="xl">
+            <div className="login-section__wrapper">
+              <div className="login-text__wrapper">
+                <h2 className="login-title">facebook</h2>
+                <p className="login-subtitle">
+                  Facebook помогает вам всегда оставаться на связи и общаться со
+                  своими знакомыми.
+                </p>
+              </div>
+              {!recentLogin
+                ? null
+                : recentLogin.map((elem, i) => (
+                    <div key={i}>
+                      <img src={elem.src} alt={elem.photo} />
+                      <p>{elem.userName}</p>
+                    </div>
+                  ))}
+              <form action="#" className="login-form">
+                <TextField
+                  name="email"
+                  onChange={loginForm.handleChange}
+                  error={!!loginForm.errors.email}
+                  onBlur={loginForm.handleBlur}
+                  value={loginForm.values.existingEmail}
+                  id="userEmail"
+                  label="Электронный адрес или номер телефона"
+                  variant="outlined"
+                  fullWidth
+                />
+                {loginForm.touched.email && loginForm.errors.email ? (
+                  <p>Неверная почта или номер телефона</p>
+                ) : null}
+                <TextField
+                  name="password"
+                  onChange={loginForm.handleChange}
+                  onBlur={loginForm.handleBlur}
+                  value={loginForm.values.existingPassword}
+                  id="password"
+                  label="Пароль"
+                  error={!!loginForm.errors.password}
+                  variant="outlined"
+                  type={showPassword ? "text" : "password"}
+                  fullWidth
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          edge="end">
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                {loginForm.touched.password && loginForm.errors.password ? (
+                  <p>Некорректный пароль</p>
+                ) : null}
+                <Button
+                  className="form-btn"
+                  variant="contained"
+                  type="submit"
+                  onClick={loginForm.handleSubmit}>
+                  Вход
+                </Button>
+                <button className="forgot-pass-btn" onClick={handleForgot}>
+                  Забыли аккаунт?
+                </button>
+                <p className="div-line__wrapper">
+                  <span className="div-line">или</span>
+                </p>
+                <Button
+                  onClick={handleRegisterModal}
+                  className="create--btn"
+                  variant="contained"
+                  color="success">
+                  Создать новый аккаунт
+                </Button>
+              </form>
             </div>
-            {!recentLogin
-              ? null
-              : recentLogin.map((elem, i) => {
-                  <div key={i}>
-                    <img src={elem.src} alt={elem.photo} />
-                    <p>{elem.userName}</p>
-                  </div>;
-                })}
-            <form action="#" className="login-form">
-              <TextField
-                name="email"
-                onChange={loginForm.handleChange}
-                error={!!loginForm.errors.email}
-                onBlur={loginForm.handleBlur}
-                value={loginForm.values.existingEmail}
-                id="userEmail"
-                label="Электронный адрес или номер телефона"
-                variant={"outlined"}
-                fullWidth
-              />
-              {loginForm.touched.email && loginForm.errors.email ? (
-                <p>Неверная почта или номер телефона</p>
-              ) : null}
-              <TextField
-                name="password"
-                onChange={loginForm.handleChange}
-                onBlur={loginForm.handleBlur}
-                value={loginForm.values.existingPassword}
-                id="password"
-                label="Пароль"
-                error={!!loginForm.errors.password}
-                variant="outlined"
-                type={showPassword ? "text" : "password"}
-                fullWidth
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        edge="end">
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              {loginForm.touched.password && loginForm.errors.password ? (
-                <p>Некоректный пароль</p>
-              ) : null}
-              <Button
-                className="form-btn"
-                variant="contained"
-                type="submit"
-                onClick={loginForm.handleSubmit}>
-                Вход
-              </Button>
-              <button className="forgot-pass-btn" onClick={handleForgot}>
-                Забыли аккаунт?
-              </button>
-              <p className="div-line__wrapper">
-                <span className="div-line">или</span>
-              </p>
-              <Button
-                onClick={handleRegisterModal}
-                className="create--btn"
-                variant="contained"
-                color="success">
-                Создать новый аккаунт
-              </Button>
-            </form>
-          </div>
-        </Container>
-      </section>
+          </Container>
+        </section>
+      )}
       <RegisterModal modal={registerModal} handleModal={handleRegisterModal} />
       <ForgotForm modal={forgotModal} handleModal={handleForgot} />
     </>
