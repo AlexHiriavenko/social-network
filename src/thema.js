9fe86e9103eb1fd727e5ab35cd829f0437a89384
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-07-17 12:31:46 +0300

    theme draft

--- src/thema.js
+++ src/thema.js
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material";
 
-export const theme = createTheme({
+export const themeDay = createTheme({
     breakpoints: {
         values: {
             xs: 0,
@@ -12,6 +12,60 @@ export const theme = createTheme({
     },
     typography: {
         fontFamily: "Segoe UI Regular",
+    },
+    palette: {
+        bgColor: {
+            main: "#FFA500",
+        },
+        textColor: {
+            main: "#A52A2A",
+        },
+        hoverColor: {
+            main: "#800080",
+        },
+    },
+    components: {
+        MuiAvatar: {
+            styleOverrides: {
+                root: {
+                    width: 32 + "px",
+                    height: 32 + "px",
+                },
+            },
+        },
+        MuiTooltip: {
+            styleOverrides: {
+                tooltip: {
+                    fontSize: "14px",
+                },
+            },
+        },
+    },
+});
+
+export const themeNight = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+    typography: {
+        fontFamily: "Segoe UI Regular",
+    },
+    palette: {
+        bgColor: {
+            main: "#FF0000",
+        },
+        textColor: {
+            main: "#0000FF",
+        },
+        hoverColor: {
+            main: "#00FF00",
+        },
     },
     components: {
         MuiAvatar: {
