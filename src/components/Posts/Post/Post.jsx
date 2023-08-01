0d687c27ed6bbc4fd3ddc8cc695a79e0cab2d2c7
Viktor Ostapenko <vk.ostapenko@outlook.com>
2023-08-01 23:34:46 +0200

    extension of the Post component for reposts, likes and transition to the user

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -1,10 +1,19 @@
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import ReplyIcon from "@mui/icons-material/Reply";
 import { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
 import { Box, Button, Typography } from "@mui/material";
 import { BlockUserImage } from "../../UserProfile/StyledComponents/ContentBlock/StyledComponents";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getPost,
+  likePost,
+  removeLikePost,
+} from "../../../redux/post.slice/post.slice";
+import { useNavigate } from "react-router-dom";
+import { getUser, setUser } from "../../../redux/user.slice/user.slice";
 
 // Post Styles
 const StyledPost = styled("li")(({ theme }) => ({
@@ -30,13 +39,23 @@ const StyledPostAuthorName = styled(Typography)(({ theme }) => ({
   fontSize: "15px",
   color: theme.palette.textColor.main,
   fontWeight: 600,
+  cursor: "pointer",
 }));
-const StyledPostContent = styled(Typography)(({ theme }) => ({
+const StyledPostDate = styled(Typography)(({ theme }) => ({
+  fontSize: "15px",
+  color: theme.palette.textColor.secondary,
+}));
+const StyledPostContent = styled(Box)(({ theme }) => ({
   color: theme.palette.textColor.main,
   fontSize: "15px",
   paddingRight: "16px",
   paddingLeft: "16px",
-  marginBottom: "16px",
+  fontFamily: "sans-serif",
+}));
+const StyledPostContentText = styled(Typography)(({ theme }) => ({
+  color: theme.palette.textColor.main,
+  fontSize: "15px",
+  marginBottom: "10px",
   fontFamily: "sans-serif",
 }));
 const StyledPostImages = styled(Box)({
@@ -125,11 +144,110 @@ const StyledPostButton = styled(Button)(({ theme }) => ({
     backgroundColor: theme.palette.buttonColor.backgroundColor,
   },
 }));
+// Repost
+const StyledRePostWrapper = styled(Box)(({ theme }) => ({
+  borderRadius: "12px",
+  border: `1px solid ${theme.palette.backgroundColor.pageSeparator}`,
+  overflow: "hidden",
+}));
 
 export default function Post(props) {
-  const { user, likes, comments, reposts, content, postImages } = props;
+  // Constats
+  const {
+    user,
+    likes,
+    comments,
+    reposts,
+    content,
+    postImages,
+    createdDate,
+    parentId,
+    id,
+  } = props;
   const photosRef = useRef(null);
+  const dispatch = useDispatch();
+  const authUser = useSelector((state) => state.user.authorizedUser);
+  const navigate = useNavigate();
+  // State
   const [photoHeight, setPhotoHeight] = useState(195);
+  const [repost, setRepost] = useState({});
+  const [isLiked, setLikedStatus] = useState(false);
+  const [likesAmount, setLikesAmount] = useState(likes.length);
+  // Functions
+  function getPostDate(postDate) {
+    const date = new Date(postDate);
+    let month;
+
+    switch (date.getMonth()) {
+      case 0:
+        month = "Jan";
+        break;
+      case 1:
+        month = "Feb";
+        break;
+      case 2:
+        month = "Mar";
+        break;
+      case 3:
+        month = "Apr";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "Jun";
+        break;
+      case 6:
+        month = "Jul";
+        break;
+      case 7:
+        month = "Aug";
+        break;
+      case 8:
+        month = "Sep";
+        break;
+      case 9:
+        month = "Oct";
+        break;
+      case 10:
+        month = "Nov";
+        break;
+      case 11:
+        month = "Dec";
+        break;
+      default:
+        month = "Invalid month number";
+    }
+
+    return month + " " + date.getDate();
+  }
+  function like() {
+    dispatch(likePost(id));
+    setLikedStatus(true);
+    setLikesAmount(likesAmount + 1);
+  }
+  function removeLike() {
+    dispatch(removeLikePost(id));
+    setLikedStatus(false);
+    setLikesAmount(likesAmount - 1);
+  }
+  function lookUser(id) {
+    if (id === authUser.id) {
+      dispatch(setUser(authUser));
+      navigate("/profile");
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      const userResponse = dispatch(getUser(id));
+      userResponse
+        .then((data) => {
+          dispatch(setUser(data.payload));
+          navigate("/profile");
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        })
+        .catch((error) => error.message);
+    }
+  }
+  // UseEffect
   useEffect(() => {
     window.addEventListener("resize", () => {
       if (photosRef.current) setPhotoHeight(photosRef.current.width);
@@ -138,6 +256,22 @@ export default function Post(props) {
   useEffect(() => {
     if (photosRef.current) setPhotoHeight(photosRef.current.width);
   }, []);
+  useEffect(() => {
+    if (!parentId) return;
+    const parentPostResponse = dispatch(getPost(parentId.id));
+    parentPostResponse
+      .then((data) => setRepost(data.payload))
+      .catch((error) => console.log(error.message));
+  }, [parentId]);
+  useEffect(() => {
+    if (!authUser) return;
+
+    if (likes.find((like) => like.id === authUser.id)) {
+      setLikedStatus(true);
+    } else {
+      setLikedStatus(false);
+    }
+  }, [repost]);
   return (
     <StyledPost>
       <StyledPostAuthor>
@@ -146,55 +280,135 @@ export default function Post(props) {
           alt="Author image"
           width={40}
           height={40}
+          onClick={() => lookUser(user.id)}
         />
-        <StyledPostAuthorName>{user.fullName}</StyledPostAuthorName>
+        <Box>
+          <StyledPostAuthorName onClick={() => lookUser(user.id)}>
+            {user.fullName}
+          </StyledPostAuthorName>
+          <StyledPostDate>{getPostDate(createdDate)}</StyledPostDate>
+        </Box>
       </StyledPostAuthor>
 
-      <StyledPostContent>{content}</StyledPostContent>
+      <StyledPostContent>
+        <StyledPostContentText>{content}</StyledPostContentText>
 
-      <StyledPostImages>
-        {postImages.length > 0 && (
-          <StyledPostImage src={postImages[0].imgUrl} alt="post image" />
+        {parentId && (
+          <StyledRePostWrapper>
+            <StyledPostImages>
+              {repost.postImages && repost.postImages.length > 0 && (
+                <StyledPostImage
+                  src={repost.postImages[0].imgUrl}
+                  alt="post image"
+                />
+              )}
+              <StyledPostExtraImages>
+                {repost.postImages &&
+                  repost.postImages.length > 0 &&
+                  repost.postImages.map((postImage, index) => {
+                    if (index === 0) return;
+                    if (index > 3) return;
+                    if (index === 3) {
+                      return (
+                        <StyledPostImageItem key={index}>
+                          <StyledPostLastShowedItem>
+                            +{repost.postImages.length - 3}
+                          </StyledPostLastShowedItem>
+                          <StyledPostImage
+                            src={postImage.imgUrl}
+                            alt="post image"
+                            ref={photosRef}
+                            width={195}
+                            height={photoHeight}
+                          />
+                        </StyledPostImageItem>
+                      );
+                    } else {
+                      return (
+                        <StyledPostImageItem key={index}>
+                          <StyledPostImage
+                            src={postImage.imgUrl}
+                            alt="post image"
+                            ref={photosRef}
+                            width={195}
+                            height={photoHeight}
+                          />
+                        </StyledPostImageItem>
+                      );
+                    }
+                  })}
+              </StyledPostExtraImages>
+            </StyledPostImages>
+            <StyledPostAuthor style={{ paddingTop: "16px" }}>
+              <BlockUserImage
+                src={repost.user && repost.user.profilePicture}
+                alt="Author image"
+                width={40}
+                height={40}
+                onClick={() => lookUser(repost.user.id)}
+              />
+              <Box>
+                <StyledPostAuthorName onClick={() => lookUser(repost.user.id)}>
+                  {repost.user && repost.user.fullName}
+                </StyledPostAuthorName>
+                <StyledPostDate>
+                  {getPostDate(repost && repost.createdDate)}
+                </StyledPostDate>
+              </Box>
+            </StyledPostAuthor>
+            <StyledPostContentText style={{ paddingLeft: "16px" }}>
+              {repost && repost.content}
+            </StyledPostContentText>
+          </StyledRePostWrapper>
         )}
-        <StyledPostExtraImages>
-          {postImages.length > 0 &&
-            postImages.map((postImage, index) => {
-              if (index === 0) return;
-              if (index > 3) return;
-              if (index === 3) {
-                return (
-                  <StyledPostImageItem key={index}>
-                    <StyledPostLastShowedItem>
-                      +{postImages.length - 3}
-                    </StyledPostLastShowedItem>
-                    <StyledPostImage
-                      src={postImage.imgUrl}
-                      alt="post image"
-                      ref={photosRef}
-                      width={195}
-                      height={photoHeight}
-                    />
-                  </StyledPostImageItem>
-                );
-              } else {
-                return (
-                  <StyledPostImageItem key={index}>
-                    <StyledPostImage
-                      src={postImage.imgUrl}
-                      alt="post image"
-                      ref={photosRef}
-                      width={195}
-                      height={photoHeight}
-                    />
-                  </StyledPostImageItem>
-                );
-              }
-            })}
-        </StyledPostExtraImages>
-      </StyledPostImages>
+      </StyledPostContent>
+
+      {!parentId && (
+        <StyledPostImages>
+          {postImages.length > 0 && (
+            <StyledPostImage src={postImages[0].imgUrl} alt="post image" />
+          )}
+          <StyledPostExtraImages>
+            {postImages.length > 0 &&
+              postImages.map((postImage, index) => {
+                if (index === 0) return;
+                if (index > 3) return;
+                if (index === 3) {
+                  return (
+                    <StyledPostImageItem key={index}>
+                      <StyledPostLastShowedItem>
+                        +{postImages.length - 3}
+                      </StyledPostLastShowedItem>
+                      <StyledPostImage
+                        src={postImage.imgUrl}
+                        alt="post image"
+                        ref={photosRef}
+                        width={195}
+                        height={photoHeight}
+                      />
+                    </StyledPostImageItem>
+                  );
+                } else {
+                  return (
+                    <StyledPostImageItem key={index}>
+                      <StyledPostImage
+                        src={postImage.imgUrl}
+                        alt="post image"
+                        ref={photosRef}
+                        width={195}
+                        height={photoHeight}
+                      />
+                    </StyledPostImageItem>
+                  );
+                }
+              })}
+          </StyledPostExtraImages>
+        </StyledPostImages>
+      )}
+
       <StyledPostReach>
         <StyledPostReachItem>
-          {likes.length > 0 ? `${likes.length} likes` : null}
+          {likes.length > 0 ? `${likesAmount} likes` : null}
         </StyledPostReachItem>
         <StyledPostReachItem>
           {comments.length > 0 ? `${comments.length} comments` : null}
@@ -204,8 +418,13 @@ export default function Post(props) {
         </StyledPostReachItem>
       </StyledPostReach>
       <StyledPostButtons>
-        <StyledPostButton>
-          <ThumbUpOffAltIcon sx={{ color: "#65676b" }} /> Like
+        <StyledPostButton onClick={isLiked ? removeLike : like}>
+          {isLiked ? (
+            <ThumbUpAltIcon sx={{ color: "#65676b" }} />
+          ) : (
+            <ThumbUpOffAltIcon sx={{ color: "#65676b" }} />
+          )}
+          Like
         </StyledPostButton>
         <StyledPostButton>
           <ChatBubbleOutlineIcon sx={{ color: "#65676b" }} /> Comment
