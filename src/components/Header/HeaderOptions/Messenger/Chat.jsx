efd3ab7cd21c329b9a0a0f5c217fa9704a028bbb
Nazar Melnik <nazar97melnik@gmail.com>
2023-08-03 14:36:38 +0300

    add some styles for chat modal

--- src/components/Header/HeaderOptions/Messenger/Chat.jsx
+++ src/components/Header/HeaderOptions/Messenger/Chat.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Paper, TextField } from "@mui/material";
+import { Avatar, Box, Paper, TextField, Typography } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import CloseIcon from "@mui/icons-material/Close";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
@@ -25,40 +25,93 @@ function Chat(props) {
           position: "fixed",
           bottom: "50px",
           backgroundColor: "#ffffff",
-          width: "30%",
+          width: "20%",
           height: "80%",
           display: "flex",
           flexDirection: "column",
           right: "50px",
+          boxShadow:
+            "0 1px 2px rgba(0, 0, 0, 0.1), 0 -1px rgba(0, 0, 0, 0.1) inset, 0 2px 1px -1px rgba(255, 255, 255, 0.5) inset",
+          border: "1px solid rgba(0, 0, 0, 0.1)",
         }}>
         {userName && userPhoto && (
-          <Box sx={{ display: "flex" }}>
-            <Avatar alt={userName} src={userPhoto} />
-            <p>{userName}</p>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+              minHeight: "40px",
+              alignItems: "center",
+              boxShadow:
+                "0 1px 2px rgba(0, 0, 0, 0.1),0 -1px rgba(0, 0, 0, 0.1) inset,0 2px 1px -1px rgba(255, 255, 255, 0.5) inset",
+            }}>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "5px",
+                justifyContent: "flex-start",
+              }}>
+              <Avatar alt={userName} src={userPhoto} />
+              <Typography variant="h6">{userName}</Typography>
+            </Box>
             <CloseIcon
               sx={{ cursor: "pointer" }}
               onClick={() => dispatch(closeChat())}
             />
           </Box>
         )}
-        {message?.map((mes, id) => (
-          <Paper key={id} elevation={3}>
-            <Box sx={{ display: "flex", gap: "10px" }}>
-              <Avatar
-                sx={{ width: 24, height: 24 }}
-                src={userPhoto}
-                alt={userName}
-              />
-              <p>{mes}</p>
-            </Box>
-          </Paper>
-        ))}
-        {answers &&
-          answers?.map((ans, id) => (
-            <Paper key={id} elevation={1}>
-              <p>{ans}</p>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            maxHeight: "80%",
+            overflowY: "scroll",
+          }}>
+          {message?.map((mes, id) => (
+            <Paper
+              key={id}
+              style={{
+                key: id,
+                display: "inline-block",
+                borderRadius: "16px",
+                padding: "10px 20px",
+                background: "#fefefe",
+                color: "0e0e0e",
+                maxWidth: "70%",
+                marginTop: "3px",
+                boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
+              }}>
+              <Box sx={{ display: "flex", gap: "10px" }}>
+                <Avatar
+                  sx={{ width: 24, height: 24 }}
+                  src={userPhoto}
+                  alt={userName}
+                />
+                <Typography>{mes}</Typography>
+              </Box>
             </Paper>
           ))}
+          {answers &&
+            answers?.map((ans, id) => (
+              <Paper
+                key={id}
+                style={{
+                  display: "inline-block",
+                  backgroundColor: "rgb(0, 132, 255)",
+                  color: "#ffffff",
+                  marginTop: "3px",
+                  borderRadius: "16px",
+                  padding: "10px 20px",
+                  background: "rgb(0, 132, 255)",
+                  maxWidth: "70%",
+                  boxShadow:
+                    "0 1px 2px rgba(0, 0, 0, 0.1), 0 -1px rgba(0, 0, 0, 0.1) inset, 0 2px 1px -1px rgba(255, 255, 255, 0.5) inset",
+                  border: "1px solid rgba(0, 0, 0, 0.1)",
+                  marginLeft: "auto",
+                }}>
+                <Typography>{ans}</Typography>
+              </Paper>
+            ))}
+        </Box>
         <Box
           sx={{
             display: "flex",
@@ -66,6 +119,9 @@ function Chat(props) {
             position: "absolute",
             bottom: 0,
             width: "100%",
+            alignItems: "center",
+            gap: "5px",
+            paddingBottom: "5px",
           }}>
           <ControlPointIcon sx={{ cursor: "pointer", color: "blue" }} />
           <TextField
--- src/pages/LogIn/RegisterModal.jsx
+++ src/pages/LogIn/RegisterModal.jsx
@@ -13,8 +13,8 @@ import { useFormik } from "formik";
 import registerValidation from "./Validation/registerValidation";
 import { Link } from "react-router-dom";
 import { dayOfBirth, yearOfBirth, mounthOfBirst } from "./registerOptions";
-import {register} from '../../redux/login.slice/login.slice'
-import {useDispatch} from 'react-redux'
+import { register } from "../../redux/login.slice/login.slice";
+import { useDispatch } from "react-redux";
 
 export default function RegisterModal(props) {
   const dispatch = useDispatch();
@@ -31,16 +31,18 @@ export default function RegisterModal(props) {
     },
     validationSchema: registerValidation,
     onSubmit: () => {
-      dispatch(register({
-        name: registerForm.values.name,
-        surname: registerForm.values.surname,
-        day: registerForm.values.day,
-        mounth: registerForm.values.mounth,
-        year: registerForm.values.year,
-        emailOrPhone: registerForm.values.emailOrPhone,
-        password: registerForm.values.password,
-        gender: registerForm.values.gender,
-      }));
+      dispatch(
+        register({
+          name: registerForm.values.name,
+          surname: registerForm.values.surname,
+          day: registerForm.values.day,
+          mounth: registerForm.values.mounth,
+          year: registerForm.values.year,
+          emailOrPhone: registerForm.values.emailOrPhone,
+          password: registerForm.values.password,
+          gender: registerForm.values.gender,
+        })
+      );
       return console.log({
         name: registerForm.values.name,
         surname: registerForm.values.surname,
@@ -67,131 +69,147 @@ export default function RegisterModal(props) {
           <p className="register-subtitle">Быстро и легко.</p>
           <Box className="register-form__row-inputs">
             <form action="#" className="register-form">
-            <TextField
-              id="name"
-              onBlur={registerForm.handleBlur}
-              error={!!registerForm.errors.name}
-              name="name"
-              required
-              label="Имя"
-              value={registerForm.values.name}
-              onChange={registerForm.handleChange}
-              variant="outlined"
-              style={{ flexGrow: "1" }}
-            />
-            <TextField
-              id="surname"
-              required
-              label="Фамилия"
-              error={!!registerForm.errors.surname}
-              name="surname"
-              value={registerForm.values.surname}
-              onChange={registerForm.handleChange}
-              variant="outlined"
-            />
+              <TextField
+                id="name"
+                onBlur={registerForm.handleBlur}
+                error={!!registerForm.errors.name}
+                name="name"
+                required
+                label="Имя"
+                value={registerForm.values.name}
+                onChange={registerForm.handleChange}
+                variant="outlined"
+                style={{ flexGrow: "1" }}
+              />
+              <TextField
+                id="surname"
+                required
+                label="Фамилия"
+                error={!!registerForm.errors.surname}
+                name="surname"
+                value={registerForm.values.surname}
+                onChange={registerForm.handleChange}
+                variant="outlined"
+              />
 
-          <TextField
-            label="Номер мобильного телефона или эл.адрес"
-            required
-            error={!!registerForm.errors.emailOrPhone}
-            variant="outlined"
-            value={registerForm.values.emailOrPhone}
-            onChange={registerForm.handleChange}
-            id="emailOrPhone"
-            name="emailOrPhone"
-            fullWidth
-          />
-          <TextField
-            label="Новый пароль"
-            id="password"
-            required
-            name="password"
-            error={!!registerForm.errors.password}
-            value={registerForm.values.password}
-            onChange={registerForm.handleChange}
-            variant="outlined"
-            type="password"
-            fullWidth
-          />
-          <FormControl>
-            <Autocomplete
-              disablePortal
-              id="day"
-              required
-              onBlur={registerForm.handleBlur}
-              name="day"
-              isOptionEqualToValue={(option, value) => option.id === value.id}
-              value={registerForm.values.day}
-              onChange={(event, newValue) => {
-                registerForm.setFieldValue("day", newValue);
-              }}
-              options={dayOfBirth}
-              renderInput={(params) => <TextField {...params} label="День" />}
-            />
-          </FormControl>
-          <FormControl>
-            <Autocomplete
-              disablePortal
-              id="mounth"
-              required
-              onBlur={registerForm.handleBlur}
-              name="mounth"
-              isOptionEqualToValue={(option, value) => option.id === value.id}
-              value={registerForm.values.mounth}
-              onChange={(event, newValue) => {
-                registerForm.setFieldValue("mounth", newValue);
-              }}
-              options={mounthOfBirst}
-              renderInput={(params) => <TextField {...params} label="Месяц" />}
-            />
-          </FormControl>
-          <FormControl>
-            <Autocomplete
-              disablePortal
-              id="year"
-              required
-              isOptionEqualToValue={(option, value) => option.id === value.id}
-              name="year"
-              value={registerForm.values.year}
-              onChange={(event, newValue) => {
-                registerForm.setFieldValue("year", newValue);
-              }}
-              options={yearOfBirth}
-              renderInput={(params) => <TextField {...params} label="Год" />}
-            />
-          </FormControl>
-          <FormControl>
-            <RadioGroup
-              row
-              aria-labelledby="gender"
-              required
-              name="gender"
-              error={!!registerForm.errors.gender}
-              id="gender"
-              value={registerForm.values.gender}
-              onChange={registerForm.handleChange}>
-              <FormControlLabel value="m" control={<Radio />} label="Мужчина" />
-              <FormControlLabel
-                value="w"
-                control={<Radio color="secondary" />}
-                label="Женщина"
+              <TextField
+                label="Номер мобильного телефона или эл.адрес"
+                required
+                error={!!registerForm.errors.emailOrPhone}
+                variant="outlined"
+                value={registerForm.values.emailOrPhone}
+                onChange={registerForm.handleChange}
+                id="emailOrPhone"
+                name="emailOrPhone"
+                fullWidth
               />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="Другое"
+              <TextField
+                label="Новый пароль"
+                id="password"
+                required
+                name="password"
+                error={!!registerForm.errors.password}
+                value={registerForm.values.password}
+                onChange={registerForm.handleChange}
+                variant="outlined"
+                type="password"
+                fullWidth
               />
-            </RadioGroup>
-          </FormControl>
-            <Button
+              <FormControl>
+                <Autocomplete
+                  disablePortal
+                  id="day"
+                  required
+                  onBlur={registerForm.handleBlur}
+                  name="day"
+                  isOptionEqualToValue={(option, value) =>
+                    option.id === value.id
+                  }
+                  value={registerForm.values.day}
+                  onChange={(event, newValue) => {
+                    registerForm.setFieldValue("day", newValue);
+                  }}
+                  options={dayOfBirth}
+                  renderInput={(params) => (
+                    <TextField {...params} label="День" />
+                  )}
+                />
+              </FormControl>
+              <FormControl>
+                <Autocomplete
+                  disablePortal
+                  id="mounth"
+                  required
+                  onBlur={registerForm.handleBlur}
+                  name="mounth"
+                  isOptionEqualToValue={(option, value) =>
+                    option.id === value.id
+                  }
+                  value={registerForm.values.mounth}
+                  onChange={(event, newValue) => {
+                    registerForm.setFieldValue("mounth", newValue);
+                  }}
+                  options={mounthOfBirst}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Месяц" />
+                  )}
+                />
+              </FormControl>
+              <FormControl>
+                <Autocomplete
+                  disablePortal
+                  id="year"
+                  required
+                  isOptionEqualToValue={(option, value) =>
+                    option.id === value.id
+                  }
+                  name="year"
+                  value={registerForm.values.year}
+                  onChange={(event, newValue) => {
+                    registerForm.setFieldValue("year", newValue);
+                  }}
+                  options={yearOfBirth}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Год" />
+                  )}
+                />
+              </FormControl>
+              <FormControl>
+                <RadioGroup
+                  row
+                  aria-labelledby="gender"
+                  required
+                  name="gender"
+                  error={!!registerForm.errors.gender}
+                  id="gender"
+                  value={registerForm.values.gender}
+                  onChange={registerForm.handleChange}>
+                  <FormControlLabel
+                    value="m"
+                    control={<Radio />}
+                    label="Мужчина"
+                  />
+                  <FormControlLabel
+                    value="w"
+                    control={<Radio color="secondary" />}
+                    label="Женщина"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Другое"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <Button
                 onClick={registerForm.handleSubmit}
                 className="form-btn create--btn "
                 variant="contained"
                 type="submit"
                 color="success"
                 fullWidth>
-              Регистрация
-            </Button>
+                Регистрация
+              </Button>
             </form>
           </Box>
           <p className="form-footer-text">
@@ -207,7 +225,6 @@ export default function RegisterModal(props) {
             файлов cookie. Вы можете получать от нас SMS-уведомления, отказаться
             от которых можно в любой момент.
           </p>
-
         </Box>
       </Modal>
     </>
