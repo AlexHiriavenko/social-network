09593155a4bb5c38ade63226a559a1ba9a802e88
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2023-08-22 16:59:09 +0300

    feat: add content of FriendBirthdays page. refactioring: commented requests to back that is not nessasary

--- src/pages/Friends/FriendBirthdays/FriendBirthdays.jsx
+++ src/pages/Friends/FriendBirthdays/FriendBirthdays.jsx
@@ -112,9 +112,6 @@ function FriendBirthdays() {
             </SectionWraper>
         </PageBoxFriends>
     </PageBoxFriendsWrapper>
-       /*  <Box sx={{width: '100%', height: '93vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Typography sx={{fontSize: 26, fontFamily: "inherit", fontWeight: 700, }}>Is coming soon...</Typography>
-        </Box> */
     )
 }
 
