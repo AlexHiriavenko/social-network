aefb309b563f8dd466031b66e11ce55bd9b88760
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-08-28 00:14:54 +0300

    add newUser to chat improove

--- src/components/Modals/AddUserToChatModal/UsersListGlobal.jsx
+++ src/components/Modals/AddUserToChatModal/UsersListGlobal.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { List, ListItem, Typography, Avatar } from "@mui/material/";
 import { Link } from "react-router-dom";
@@ -9,60 +10,80 @@ import { getChat, addToChatNewUser } from "../../../redux/chat.slice/chat.slice"
 function UsersListGlobal(props) {
     const dispatch = useDispatch();
     const theme = useTheme();
+    const [alertMessage, setAlertMessage] = useState(false);
 
     const authUser = useSelector((state) => state.user.authorizedUser);
     const currentChat = useSelector((state) => state.chat.currentChat);
-    console.log(currentChat);
+    const { users } = currentChat;
+    const userIdies = users.map((user) => user.id);
+    const isNewUser = (id) => !userIdies.includes(+id);
 
     function handleAddUser(id = 0, fullName, newUserId) {
-        const newMessage = {
-            id: id,
-            content: `${authUser.fullName} add to chat new participant: ${fullName}`,
-            sender: authUser,
-            chat: currentChat,
-        };
-        console.log(`url запрос на /chats/${currentChat.id}/participants/${newUserId}`);
-
-        dispatch(addToChatNewUser({ chatId: currentChat.id, userId: newUserId }))
-            .then(() => dispatch(closeAddUserToChatModal()))
-            .then(() => dispatch(sendMessage(newMessage)))
-            .then(() => dispatch(getChat(currentChat.id)))
-            .catch((error) => {
-                console.error("Error sending message:", error);
-            });
+        if (isNewUser(newUserId)) {
+            setAlertMessage(false);
+            const newMessage = {
+                id: id,
+                content: `${authUser.fullName} add to chat new participant: ${fullName}`,
+                sender: authUser,
+                chat: currentChat,
+            };
+            dispatch(addToChatNewUser({ chatId: currentChat.id, userId: newUserId }))
+                .then(() => dispatch(closeAddUserToChatModal()))
+                .then(() => dispatch(sendMessage(newMessage)))
+                .then(() => dispatch(getChat(currentChat.id)))
+                .catch((error) => {
+                    console.error("Error sending message:", error);
+                });
+        } else {
+            setAlertMessage(true);
+        }
     }
 
+    useEffect(() => {
+        return () => {
+            setAlertMessage(false);
+        };
+    }, []);
+
     return (
-        <List sx={{ minHeight: "340px" }}>
-            {props.users?.map((user) => (
-                <ListItem
-                    key={user.id}
-                    sx={{
-                        cursor: "pointer",
-                        gap: 1,
-                        "&:hover": {
-                            backgroundColor: theme.palette.hoverColor.main,
-                        },
-                    }}
-                    onClick={() => handleAddUser(0, user.fullName, +user.id)}
-                >
-                    <Link className="search__user-link">
-                        <Avatar
-                            className="search__user-avatar"
-                            sx={{ minWidth: "40px", minHeight: "40px" }}
-                            alt="user icon"
-                            src={user.profilePicture}
-                        ></Avatar>
-                        <Typography
-                            className="search__user-name"
-                            color={theme.palette.textColor.content}
-                        >
-                            {user.fullName}
-                        </Typography>
-                    </Link>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            {alertMessage && (
+                <Typography color={"error"} align="center">
+                    This user is already in the chat! It is not possible to add an existing user
+                    again.
+                </Typography>
+            )}
+            <List sx={{ minHeight: "340px" }}>
+                {props.users?.map((user) => (
+                    <ListItem
+                        key={user.id}
+                        sx={{
+                            cursor: "pointer",
+                            gap: 1,
+                            "&:hover": {
+                                backgroundColor: theme.palette.hoverColor.main,
+                            },
+                        }}
+                        onClick={() => handleAddUser(0, user.fullName, +user.id)}
+                    >
+                        <Link className="search__user-link">
+                            <Avatar
+                                className="search__user-avatar"
+                                sx={{ minWidth: "40px", minHeight: "40px" }}
+                                alt="user icon"
+                                src={user.profilePicture}
+                            ></Avatar>
+                            <Typography
+                                className="search__user-name"
+                                color={theme.palette.textColor.content}
+                            >
+                                {user.fullName}
+                            </Typography>
+                        </Link>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     );
 }
 
