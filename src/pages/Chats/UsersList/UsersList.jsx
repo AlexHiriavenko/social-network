16ecfb5b19869c99fb6caffb05863237851ad5d2
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-09-01 22:55:16 +0300

    chats in progress

--- src/pages/Chats/UsersList/UsersList.jsx
+++ src/pages/Chats/UsersList/UsersList.jsx
@@ -10,7 +10,6 @@ import { List, ListItem, Typography, Avatar, Box } from "@mui/material/";
 import { Link } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import { deleteTemporaryParticipant } from "../../../redux/chat.slice/chat.slice";
-import filterChatParticipants from "../helpers/filterChatParticipants";
 
 function UsersList(props) {
     const { setNewMessageDialog } = props;
@@ -18,40 +17,7 @@ function UsersList(props) {
     const theme = useTheme();
 
     const authUserID = useSelector((state) => state.user.authorizedUser.id);
-    const chatParticipants = useSelector(
-        (state) => state.chat.chatsParticipants
-    );
-
-    const filteredChats = filterChatParticipants(chatParticipants, authUserID);
-
-    // function filteredParticpents() {
-    //     if (chatParticipants.length > 0) {
-    //         const filteredParticpents = chatParticipants.filter(
-    //             (chat) => chat.userId !== authUserID
-    //         );
-
-    //         const uniqueIdMap = new Map();
-    //         const idCountMap = {};
-
-    //         filteredParticpents.forEach((item) => {
-    //             if (!uniqueIdMap.has(item.id)) {
-    //                 uniqueIdMap.set(item.id, item);
-    //                 idCountMap[item.id] = 0;
-    //             } else {
-    //                 idCountMap[item.id] += 1;
-    //             }
-    //         });
-
-    //         const clone = structuredClone(uniqueIdMap);
-
-    //         clone.forEach((item) => {
-    //             item.quantityUsers = idCountMap[item.id];
-    //         });
-
-    //         return Array.from(clone.values());
-    //     }
-    //     return [];
-    // }
+    const chatParticipants = useSelector((state) => state.chat.chatsParticipants);
 
     const currentChat = useSelector((state) => state.chat.currentChat);
 
@@ -62,11 +28,7 @@ function UsersList(props) {
         if (chatId) {
             const closestLi = event.target.closest("li");
             const userId = +closestLi.id.slice(8);
-            dispatch(
-                setCurrentChatCompanion(
-                    chatParticipant(chatParticipants, userId)
-                )
-            );
+            dispatch(setCurrentChatCompanion(chatParticipant(chatParticipants, userId)));
             dispatch(getChat(chatId));
             dispatch(openPageChat());
             dispatch(deleteTemporaryParticipant());
@@ -81,24 +43,16 @@ function UsersList(props) {
     return (
         <List
             className="users-list"
-            sx={{ minHeight: "50px", maxHeight: "80vh", overflowY: "auto" }}>
+            sx={{ minHeight: "50px", maxHeight: "80vh", overflowY: "auto" }}
+        >
             {!chatParticipants.length && (
-                <Typography
-                    sx={{ p: 2 }}
-                    color={theme.palette.textColor.content}>
+                <Typography sx={{ p: 2 }} color={theme.palette.textColor.content}>
                     No history yet
                 </Typography>
             )}
-            {!!filteredChats.length &&
-                filteredChats.map(
-                    ({
-                        id: chatId,
-                        profilePicture,
-                        fullName,
-                        userId,
-                        content,
-                        quantityUsers,
-                    }) => (
+            {!!chatParticipants.length &&
+                chatParticipants.map(
+                    ({ id: chatId, profilePicture, fullName, userId, content, quantityUsers }) => (
                         <ListItem
                             id={`chatUser${userId}`}
                             key={chatId}
@@ -109,26 +63,26 @@ function UsersList(props) {
                                     : "none",
                                 gap: 1,
                                 "&:hover": {
-                                    backgroundColor:
-                                        theme.palette.hoverColor.secondary,
+                                    backgroundColor: theme.palette.hoverColor.secondary,
                                 },
                             }}
-                            className="chats__list-item">
+                            className="chats__list-item"
+                        >
                             <Link className="search__user-link">
                                 <Avatar
                                     className="search__user-avatar"
                                     sx={{ minWidth: "40px", minHeight: "40px" }}
                                     alt="user icon"
-                                    src={profilePicture}></Avatar>
+                                    src={profilePicture}
+                                ></Avatar>
                                 <Box className="searh__user-text">
                                     <Typography
                                         className="search__user-name"
-                                        color={theme.palette.textColor.content}>
+                                        color={theme.palette.textColor.content}
+                                    >
                                         {fullName}{" "}
                                         {quantityUsers > 0 && (
-                                            <Typography
-                                                variant="span"
-                                                sx={{ fontSize: "13px" }}>
+                                            <Typography variant="span" sx={{ fontSize: "13px" }}>
                                                 & {quantityUsers} more
                                             </Typography>
                                         )}
@@ -142,7 +96,8 @@ function UsersList(props) {
                                             whiteSpace: "nowrap",
                                             overflow: "hidden",
                                             textOverflow: "ellipsis",
-                                        }}>
+                                        }}
+                                    >
                                         {content}
                                     </Typography>
                                 </Box>
