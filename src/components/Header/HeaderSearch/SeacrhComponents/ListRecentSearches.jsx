49a0cb4065d606b0130dfde6b805129ee4d79694
Valeria Vitvytska <leranmu@gmail.com>
2023-09-03 19:04:02 +0300

    show profile page from homepage search if authorized user (#109)
    * fix:removeInterval
    
    * fix:make async functions establish env variables as url for Google auth endpoints
    
    * fix:some fixing for token renewal
    
    * fix:remove baseFunc
    
    * fix:fix google auth for safari browser
    
    * fix:update savePicture in editChildModal
    
    * fix:update savePicture in editChildModal
    
    * fix:update savePicture in editChildModal
    
    * fix:update instance
    
    * feat:add action to find user by part of name
    
    * feat:add action to find user by part of name
    
    * fix:fix uploadAvatar endpoint
    
    * fix: second fix uploadAvatar endpoint
    
    * feat: upload user images
    
    * feat: upload user images
    
    * feat:show user images on profile page
    
    * feat:update instance create SearchForHomePage
    
    * fix:merge develop
    
    * feat :improve Search for HomePage & authorizatio Google
    
    * feat :improve Search for HomePage & authorization Google
    
    * feat:add ProfileForSearch
    
    * fix:merge develop
    
    * fix:update ProfileForSearch
    
    * feat: user profile for search of users
    
    * feat: user profile for search of users
    
    * fix: improve user profile for search of users
    
    * fix: improve user profile for search of users
    
    * fix:fix ListRecentSearches
    
    * fix:update ProfileHeaderForSearch
    
    * fix:auth google
    
    * fix:update styles for forgot form close registration form after submit
    
    * fix:update Home
    
    * fix:show friends of User on user profile for search
    
    * fix:show friends from friendlistforsearch
    
    * fix:update userfriends in headerforsearch
    
    * fix:update profile header for search
    
    * fix:update logout
    
    * feat:activate edit cover photo and edit profile photo buttons
    
    * feat:activate edit cover photo and edit profile photo buttons
    
    * feat:upload list of user images
    
    * feat:show error message if incorrect login or password
    
    * feat:show profile of authorized user when authorized found in search
    
    * feat:show profile picture from search if user authorized
    
    * feat: show profile page from search if user authorized
    
    * feat: show profile page from search if user authorized
    
    * feat: show red error message hide password i ChangePasswordForm

--- src/components/Header/HeaderSearch/SeacrhComponents/ListRecentSearches.jsx
+++ src/components/Header/HeaderSearch/SeacrhComponents/ListRecentSearches.jsx
@@ -1,12 +1,30 @@
 import { List, ListItem, Typography, Avatar } from "@mui/material/";
 import { Link } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getFriends, setFriends, setUser} from "../../../../redux/user.slice/user.slice.js";
 
 function ListRecentSearches(props) {
     const theme = useTheme();
     const onClick = props.onClick;
+    const dispatch = useDispatch();
     const authUser = useSelector((state) => state.user.authorizedUser);
+    const user = useSelector((state) => state.user.user);
+    useEffect(()=>{
+        if(user === null){
+        dispatch(setUser(authUser));
+            const userFriendsResponse = dispatch(getFriends(authUser.id));
+            userFriendsResponse
+                .then((data) => {
+                    if (data.payload) {
+                        dispatch(setFriends(data.payload));
+                        localStorage.setItem("friends", JSON.stringify(data.payload));
+                    }
+                })
+                .catch((error) => console.log(error.message));
+        }
+    },[])
 
     return (
         <List>
