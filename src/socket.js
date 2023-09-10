a8332d5358ded91a124efe4eb50b6d060b2fb793
TetianaPysariuha <108232607+TetianaPysariuha@users.noreply.github.com>
2023-09-10 12:56:00 +0300

    Notifications (#124)
    * main friend page and frien slice
    
    * svg components
    
    * adjust colors to theme
    
    * feat: receive data from backend by token instead userId
    
    * feat: add userFriendPage
    
    * delete nonusing imported elements
    
    * refactor: add proptypes to SideBarFriends.jsx
    
    * fix: work on color theme
    
    * fix: violations
    
    * fix: violations
    
    * fix: violations
    
    * fix: violations
    
    * fix: violations
    
    * fix: post new friend from suggestions
    
    * fix: post new friend from suggestions
    
    * feat: adjust colors to theme
    
    * code refactoring
    
    * function for get data for userProfile
    
    * feat: set user for render profile in fried pages
    
    * fix: set maxWidth or friends pages, minWidth of sidebar, background for all page for big screens
    
    * fixed: rendering friends when choose on sidebar
    
    * feat: sent requests modal window. fix: added stopPropagation to hadle function
    
    * merge with develop branch
    
    * add: search element, popum menu for useFfriends page, new actions to friendSlice
    
    * fix small inaccuracies
    
    * add blank frien birthdays page
    
    * merge develop
    
    * fix: cleare current friend when it deleted
    
    * feat: add content of FriendBirthdays page. refactioring: commented requests to back that is not nessasary
    
    * feat: add content of FriendBirthdays page. refactioring: commented requests to back that is not nessasary
    
    * add console writer for debugging
    
    * fix: buttons remove and add on friendSugestions page
    
    * fix: search component, refactoring
    
    * fix: search component, refactoring
    
    * fix: search element change logic for placeholder text
    
    * fix: color of link on FriendBirthday page, height of friend pages
    
    * fix: decoration of sidebar for friends
    
    * feat: adjust all friend pages for small screen
    
    * fix: delete all console.log in friends pages
    
    * fix: allFriend page not fount
    
    * fix: allFriend page not fount
    
    * fix: allFriend page not fount
    
    * feat: add new buttons fo redirect on friends pages
    
    * fix: friendHome page on small size change placement of buttons
    
    * feat: buttom add friend on profile page on click logic create
    
    * feat: added connection to socket and subscriptions to topics
    
    * fix: add control subscribe  withaout doubling, unsubscribe all existant subscribes
    
    * feat: add notificztion saving to slise
    
    * fix: stabilisated connection to socket

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,8 @@ import Stomp from 'stompjs';
 
 let stompClient;
 let socket;
-let listOfChanales;
+let listOfChanales = [];
+
 export const subscribes = [];
 const serverUrl = `${import.meta.env.VITE_APP_API_URL}/websocket-endpoint`;
 export let isConnected = false;
@@ -14,35 +15,39 @@ export const connectWebSocket = (inputListOfChanales) => {
   };
 
 const connectSocket = () => {
-    if (!socket) {
+    if (!socket || socket.readyState === WebSocket.CLOSED) {
         socket = new SockJS(serverUrl);
         stompClient = Stomp.over(socket);    
-    }
     
-    stompClient.connect({}, (frame) => {
-        console.log('Connected to WebSocket');
-        isConnected = true;
-        
-        listOfChanales.forEach(element => {
-            if(subscribes.filter(el => el.topic === element.topic).length > 0) {
-                return;
-            }
-            const id = stompClient.subscribe(element.topic, (message) => element.callback(message))
-            subscribes.push({topic: element.topic, id});
+        stompClient.connect({}, (frame) => {
+            console.log(frame);
+            isConnected = true;
+            
+            listOfChanales.forEach(element => {
+                if(subscribes.filter(el => el.topic === element.topic).length > 0) {
+                    return;
+                }
+                const id = stompClient.subscribe(element.topic, (message) => element.callback(message))
+                subscribes.push({topic: element.topic, id});
+            });
         });
-      });
+    }
       
     socket.onClose = (event) => {
+        console.log('onClose');
         console.log(event);
         isConnected = false;
-        connectSocket();
+        if (listOfChanales.length > 0) {
+            connectSocket();
+        }
     } 
 }
 
 export const disconnectWebSocket = () => {
-    stompClient.disconnect();
-    console.log('WebSocket connection closed');
-    subscribes.forEach(el => stompClient.unsubscribe(el.id.id));
-    subscribes.splice(0);
-    isConnected = false;
+    stompClient.disconnect(() => {
+        subscribes.forEach(el => stompClient.unsubscribe(el.id.id));
+        subscribes.splice(0);
+        isConnected = false;
+        listOfChanales = [];
+    });
 };
