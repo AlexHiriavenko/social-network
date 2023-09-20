d1fc35a2ba228c9f73619d6e7ca4b9217e91b08f
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-09-20 19:17:55 +0300

    fix home

--- src/pages/Home/HomeMain.jsx
+++ src/pages/Home/HomeMain.jsx
@@ -2,10 +2,7 @@ import { useSelector } from "react-redux";
 import PostList from "../../components/Posts/Post/PostList";
 import { useEffect, useState } from "react";
 import CreatePost from "../../components/Posts/CreatePost";
-import {
-    getPageblePosts,
-    setVisiblePosts,
-} from "../../redux/post.slice/post.slice.js";
+import { getPageblePosts, setVisiblePosts } from "../../redux/post.slice/post.slice.js";
 import { useDispatch } from "react-redux";
 
 function HomeMain() {
@@ -18,11 +15,7 @@ function HomeMain() {
     const dispatch = useDispatch();
     // Functions
     function handleScroll(e) {
-        if (
-            e.target.documentElement.scrollHeight -
-                (e.target.documentElement.scrollTop + window.innerHeight) <
-            250
-        ) {
+        if (e.target.scrollHeight - (e.target.scrollTop + e.target.offsetHeight) < 250) {
             setFetching(true);
         }
     }
@@ -33,56 +26,7 @@ function HomeMain() {
                 .then((data) => {
                     setCurrentPage(currentPage + 1);
                     setMainPagePosts([...mainPagePosts, ...data.payload]);
-                    dispatch(
-                        setVisiblePosts([...mainPagePosts, ...data.payload])
-                    );
-                })
-                .catch((error) => console.log(error))
-                .finally(() => setFetching(false));
-        }
-    }, [fetching]);
-    useEffect(() => {
-        document.addEventListener("scroll", handleScroll);
-        return () => {
-            document.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    useEffect(() => {
-        setMainPagePosts(visiblePosts);
-    }, [visiblePosts]);
-    return (
-        <main className="main-home-content">
-            <CreatePost />
-            <PostList posts={visiblePosts} />
-        </main>
-    );
-    // State
-    const [mainPagePosts, setMainPagePosts] = useState([]);
-    const [fetching, setFetching] = useState(true);
-    const [currentPage, setCurrentPage] = useState(0);
-    // Constants
-    const visiblePosts = useSelector((state) => state.post.visiblePosts);
-    const dispatch = useDispatch();
-    // Functions
-    function handleScroll(e) {
-        if (
-            e.target.scrollHeight -
-                (e.target.scrollTop + e.target.offsetHeight) <
-            250
-        ) {
-            setFetching(true);
-        }
-    }
-    //   useEffect
-    useEffect(() => {
-        if (fetching) {
-            dispatch(getPageblePosts({ page: currentPage, size: 6 }))
-                .then((data) => {
-                    setCurrentPage(currentPage + 1);
-                    setMainPagePosts([...mainPagePosts, ...data.payload]);
-                    dispatch(
-                        setVisiblePosts([...mainPagePosts, ...data.payload])
-                    );
+                    dispatch(setVisiblePosts([...mainPagePosts, ...data.payload]));
                 })
                 .catch((error) => console.log(error))
                 .finally(() => setFetching(false));
