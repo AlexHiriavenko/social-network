e7e8c4dd702fb98c53479dd0d2e4448945b8959d
leradent1989 <leranmu@gmail.com>
2023-09-21 10:50:37 +0300

    feat:update about info

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,17 +6,14 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-    getAccessToken,
-    loginGoogle, logOut,
+
+    loginGoogle,
 } from "../../redux/login.slice/login.slice.js";
 import {
-    getUser,
     getProfile,
-    getUsers,
     setAuthorizedUser,
-    setUsers,findByPartOfName,
 } from "../../redux/user.slice/user.slice.js";
-import { getPosts, setPosts } from "../../redux/post.slice/post.slice.js";
+
 
 
 function Home() {
@@ -24,10 +21,6 @@ function Home() {
 
     const dispatch = useDispatch();
     let user = useSelector((state) => state.user.authorizedUser);
-
-  //  if(!JSON.parse(localStorage.getItem("token"))){
-   //     dispatch(logOut())
-   // }
 
     useEffect(() => {
 
@@ -37,9 +30,10 @@ function Home() {
                 let result = await dispatch(getProfile())
 
                     dispatch(setAuthorizedUser({...result.payload,isAuthorized:true}))
-                   // localStorage.setItem('authorizedUser',JSON.stringify({...result.payload,isAuthorized:true}))
+
             }
             )()
+
         }
         if (
             !localStorage.getItem("authorizedUser") &&
@@ -77,17 +71,6 @@ function Home() {
                 )
             );
         }
-
-       if (!user) {
-            (async()=>{
-
-                    let result = await dispatch(getProfile())
-
-                    dispatch(setAuthorizedUser({...result.payload,isAuthorized:true}))
-                }
-            )()
-        }
-
 
     }, []);
 
