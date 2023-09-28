a5d64b4c656b7caa438f5abb0010882ce75fc0ef
Valeria Vitvytska <leranmu@gmail.com>
2023-09-29 02:33:44 +0300

    Update Login.jsx

--- src/pages/LogIn/Login.jsx
+++ src/pages/LogIn/Login.jsx
@@ -38,7 +38,7 @@ export default function LogIn() {
 
   useEffect(() => {
     
-      if(loggedIn ){
+      if(loggedIn && JSON.parse(localStorage.getItem("token")) == "out"){
 
       dispatch(logOut());
     }    
