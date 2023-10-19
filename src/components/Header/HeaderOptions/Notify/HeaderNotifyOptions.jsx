41c33260d3d3ccc60c495dca0b0c33061f383f31
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-10-19 23:13:26 +0300

    mini fix nofication drop-menu size

--- src/components/Header/HeaderOptions/Notify/HeaderNotifyOptions.jsx
+++ src/components/Header/HeaderOptions/Notify/HeaderNotifyOptions.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
-import {
-    IconButton,
-    Typography,
-    Menu,
-    Avatar,
-    Tooltip,
-    Badge,
-    Box,
-} from "@mui/material";
+import { IconButton, Typography, Menu, Avatar, Tooltip, Badge, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import {
-    AvatarStyled,
-    BtnNotifyStyled,
-    MenuItemStyled,
-} from "../headerOptionsStyled";
+import { AvatarStyled, BtnNotifyStyled, MenuItemStyled } from "../headerOptionsStyled";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "@emotion/styled";
 import { openCreateCommentModal } from "../../../../redux/modal.slice/modal.slice";
 import { getPost } from "../../../../redux/post.slice/post.slice";
-import { addToAllNotifications, getNotificationsSize, getPagebleNotifications, setAllNotifications, setViewNotification } from "../../../../redux/notifications.slice/notifications.slice";
+import {
+    addToAllNotifications,
+    getNotificationsSize,
+    getPagebleNotifications,
+    setAllNotifications,
+    setViewNotification,
+} from "../../../../redux/notifications.slice/notifications.slice";
 
 const StyledUnreadedPoint = styled("div")(({ theme }) => ({
     width: "13px",
@@ -40,32 +34,35 @@ function HeaderNotifyOptions() {
         anchorNotifyMenu
             ? setAnchorNotifyMenu(null)
             : setAnchorNotifyMenu(document.querySelector(".anchor-menu"));
-    const newNotifications = useSelector(store => store.notifications.newNotifications);
-    const unreadedNotificationsSize = useSelector(store => store.notifications.unreadedNotificationsSize);
-    const allNotifications = useSelector(store => store.notifications.allNotifications);
-    const isLoading = useSelector(store => store.notifications.isLoading);
+    const newNotifications = useSelector((store) => store.notifications.newNotifications);
+    const unreadedNotificationsSize = useSelector(
+        (store) => store.notifications.unreadedNotificationsSize
+    );
+    const allNotifications = useSelector((store) => store.notifications.allNotifications);
+    const isLoading = useSelector((store) => store.notifications.isLoading);
     const dispatch = useDispatch();
 
     function checkNotification(notification) {
-        dispatch(getPost(notification.updatedEntityId)).then(data => {
+        dispatch(getPost(notification.updatedEntityId)).then((data) => {
             if (notification.status === "pending") {
-                dispatch(setViewNotification({ notificationId: notification.id }))
+                dispatch(setViewNotification({ notificationId: notification.id }));
                 dispatch(getNotificationsSize());
             }
             dispatch(openCreateCommentModal(data.payload));
-        })
+        });
     }
 
     function handleScroll(e) {
         if (
-            e.target.scrollHeight - (e.target.scrollTop + e.target.offsetHeight) < 100 && !isFetching
+            e.target.scrollHeight - (e.target.scrollTop + e.target.offsetHeight) < 100 &&
+            !isFetching
         ) {
             setIsFetching(true);
         }
     }
 
     function togleFilterNotification(isAllNotifications) {
-        setIsAllNotifications(isAllNotifications)
+        setIsAllNotifications(isAllNotifications);
         dispatch(setAllNotifications([]));
         setCurrentPage(0);
         setIsFetching(true);
@@ -74,7 +71,7 @@ function HeaderNotifyOptions() {
     useEffect(() => {
         if (isFetching && !isLoading) {
             const status = isAllNotifications ? "all" : "pending";
-            dispatch(getPagebleNotifications({ page: currentPage, size: 5, status }))
+            dispatch(getPagebleNotifications({ page: currentPage, size: 5, status }));
             setCurrentPage(currentPage + 1);
         }
         setIsFetching(false);
@@ -82,19 +79,21 @@ function HeaderNotifyOptions() {
 
     useEffect(() => {
         dispatch(getNotificationsSize());
-    }, [])
+    }, []);
 
     useEffect(() => {
-        if (newNotifications[0]) dispatch(addToAllNotifications(newNotifications[newNotifications.length - 1]));
-    }, [newNotifications])
+        if (newNotifications[0])
+            dispatch(addToAllNotifications(newNotifications[newNotifications.length - 1]));
+    }, [newNotifications]);
 
     return (
         <>
             <Tooltip title="Notifications" sx={{ p: { xs: "4px", sm: 1 } }}>
-                <IconButton
-                    onClick={toggleMenu}
-                    sx={({ py: 1 }, { px: { xs: 0.5, sm: 1 } })}>
-                    <Badge badgeContent={unreadedNotificationsSize + newNotifications.length} color="secondary">
+                <IconButton onClick={toggleMenu} sx={({ py: 1 }, { px: { xs: 0.5, sm: 1 } })}>
+                    <Badge
+                        badgeContent={unreadedNotificationsSize + newNotifications.length}
+                        color="secondary"
+                    >
                         <AvatarStyled>
                             <NotificationsIcon
                                 style={{
@@ -125,8 +124,8 @@ function HeaderNotifyOptions() {
                         onScroll: handleScroll,
                         className: "header__options-drop-menu",
                         style: {
-                            backgroundColor:
-                                theme.palette.backgroundColor.section,
+                            backgroundColor: theme.palette.backgroundColor.section,
+                            minHeight: "400px",
                         },
                     },
                 }}
@@ -137,7 +136,8 @@ function HeaderNotifyOptions() {
                     px={2}
                     py={1}
                     fontWeight={600}
-                    sx={{ color: theme.palette.textColor.content }}>
+                    sx={{ color: theme.palette.textColor.content }}
+                >
                     Notifications
                 </Typography>
                 <Box display={"flex"} gap={1} mb={2} px={2}>
@@ -156,41 +156,51 @@ function HeaderNotifyOptions() {
                     if (!isAllNotifications) {
                         if (notification.status === "pending") {
                             return (
-                                <MenuItemStyled key={index} onClick={() => {
-                                    toggleMenu();
-                                    checkNotification(notification);
-                                }}>
+                                <MenuItemStyled
+                                    key={index}
+                                    onClick={() => {
+                                        toggleMenu();
+                                        checkNotification(notification);
+                                    }}
+                                >
                                     <Avatar
                                         sx={{ minWidth: "40px", minHeight: "40px" }}
                                         alt="user icon"
-                                        src={notification.sender.profilePicture}></Avatar>
+                                        src={notification.sender.profilePicture}
+                                    ></Avatar>
                                     <Typography
                                         fontSize={15}
-                                        sx={{ color: theme.palette.textColor.content }}>
+                                        sx={{ color: theme.palette.textColor.content }}
+                                    >
                                         <b>{notification.sender.fullName}</b> {notification.content}
                                     </Typography>
                                     {notification.status === "pending" && <StyledUnreadedPoint />}
                                 </MenuItemStyled>
-                            )
+                            );
                         }
                     } else {
                         return (
-                            <MenuItemStyled key={index} onClick={() => {
-                                toggleMenu();
-                                checkNotification(notification);
-                            }}>
+                            <MenuItemStyled
+                                key={index}
+                                onClick={() => {
+                                    toggleMenu();
+                                    checkNotification(notification);
+                                }}
+                            >
                                 <Avatar
                                     sx={{ minWidth: "40px", minHeight: "40px" }}
                                     alt="user icon"
-                                    src={notification.sender.profilePicture}></Avatar>
+                                    src={notification.sender.profilePicture}
+                                ></Avatar>
                                 <Typography
                                     fontSize={15}
-                                    sx={{ color: theme.palette.textColor.content }}>
+                                    sx={{ color: theme.palette.textColor.content }}
+                                >
                                     <b>{notification.sender.fullName}</b> {notification.content}
                                 </Typography>
                                 {notification.status === "pending" && <StyledUnreadedPoint />}
                             </MenuItemStyled>
-                        )
+                        );
                     }
                 })}
             </Menu>
